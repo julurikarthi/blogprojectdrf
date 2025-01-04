e01fba0727a255d32193725dbf5cae66440d1bca
karthik juluri <karthik.juluri@publix.com>
2025-01-04 08:57:36 -0500

    djanfo ne

---
+++ app.py
@@ -0,0 +1,9 @@
+from django.http import JsonResponse
+
+def data_view(request):
+    data = {
+        "message": "Hello, World!",
+        "status": "success",
+        "data": {"id": 1, "name": "Sample Item"}
+    }
+    return JsonResponse(data)
