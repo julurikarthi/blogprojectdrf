edc3d69a44af7ee587b6d2acc3f72eee220dda36
karthik juluri <karthik.juluri@publix.com>
2025-01-04 08:41:30 -0500

    djanfo ne

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from django.contrib import admin
+from . import api
 
 
 urlpatterns = [
     path('account/', include('Account.urls')),
     path('blog/', include('blogapp.urls')),
-    path('api/data/', data_view, name='api-data'),
+    path('api/data/', api.data_view, name='api-data'),
 ]
 
 
--- app.py
+++ app.py
@@ -1,17 +1,9 @@
-# app.py
-from flask import Flask, jsonify
+from django.http import JsonResponse
 
-app = Flask(__name__)
-
-@app.route('/api/data', methods=['GET'])
-def get_data():
-    # Sample JSON response
+def data_view(request):
     data = {
         "message": "Hello, World!",
         "status": "success",
         "data": {"id": 1, "name": "Sample Item"}
     }
-    return jsonify(data)
-
-if __name__ == '__main__':
-    app.run(debug=True)  # Use Gunicorn in production
+    return JsonResponse(data)
